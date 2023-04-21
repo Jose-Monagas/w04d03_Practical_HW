20b258ae058e5f0caf239e3a75aee5aab665a1a3
Jose <josemonagas212@gmail.com>
2023-04-20 22:03:19 -0400

    Adding finished task 1

---
+++ homework.js
@@ -0,0 +1,26 @@
+// Task 1: Create Hamster Class
+
+class Hamster {
+  constructor(name) {
+    this.owner = "";
+    this.name = name;
+    this.price = 15;
+  }
+
+  wheelRun() {
+    console.log("squeak squeak");
+  }
+
+  eatFood() {
+    console.log("nibble nibble");
+  }
+
+  getPrice() {
+    return this.price;
+  }
+}
+
+// test the code
+
+const newHamster = new Hamster("mr. bean");
+console.log(newHamster);
